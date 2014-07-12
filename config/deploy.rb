5f5b637009a3fe0bef35b340e07f23288ab0ebaf
tripl3inf <tripl3infinity@gmail.com>
2014-07-12 13:14:54 -0500

    adding back config folder

---
+++ config/deploy.rb
@@ -0,0 +1,36 @@
+set :application, 'ten10'
+set :repo_url, 'git@github.com:tripl3inf/ten10.git'
+
+# Branch options
+# Prompts for the branch name (defaults to current branch)
+#ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+
+# Sets branch to current one
+#set :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+
+# Hardcodes branch to always be master
+# This could be overridden in a stage config file
+set :branch, :master
+
+set :deploy_to, "/var/www/CLIENT/#{fetch(:application)}"
+
+set :log_level, :debug
+set :pty, true
+
+set :linked_files, %w{.env}
+set :linked_dirs, %w{web/app/uploads}
+#set :theme_path, "#{release_path}/app/themes/ten10_theme"
+
+namespace :deploy do
+  desc 'Restart application'
+  task :restart do # doesnt do anything, but is req by ruby app
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      execute :service, :nginx, :reload
+    end
+  end
+end
+
+# The above restart task is not run by default
+# Uncomment the following line to run it on deploys if needed
+# after 'deploy:publishing', 'deploy:restart'
---
+++ config/deploy/staging.rb
@@ -0,0 +1,23 @@
+set :stage, :staging
+
+# Simple Role Syntax
+# ==================
+#role :app, %w{deploy@t3inf.com}
+#role :web, %w{deploy@example.com}
+#role :db,  %w{deploy@example.com}
+
+# Extended Server Syntax
+# ======================
+server 't3inf.com', user: 'deploy', roles: %w{web app db}
+
+# you can set custom ssh options
+# it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
+# you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
+# set it globally
+
+set :ssh_options, {
+ forward_agent: true
+}
+
+fetch(:default_env).merge!(wp_env: :staging)
+
