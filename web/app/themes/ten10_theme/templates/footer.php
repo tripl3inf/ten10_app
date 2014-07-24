e6ad696d84fb7c8945bae1488786b306a0a488d0
tripl3inf <tripl3infinity@gmail.com>
2014-07-23 21:40:38 -0500

    adding svg logo and navbar tri, also static placeholder for feature widget in header

--- web/app/themes/ten10_theme/templates/footer.php
+++ web/app/themes/ten10_theme/templates/footer.php
@@ -1,7 +1,14 @@
 <footer class="content-info" role="contentinfo">
-  <div class="container">
+<!--  <div class="container">-->
     <?php dynamic_sidebar('sidebar-footer'); ?>
-  </div>
+<!--  </div>-->
 </footer>
+
+
+
+
+<!--</div>-->
+<!-- END main contiainer -- started in header.php -->
+
 
 <?php wp_footer(); ?>
