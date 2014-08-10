50fbbba223ec190ad03c88e95de0629337e8b298
Alex Aloia <tripl3infinity@gmail.com>
2014-08-10 14:58:37 -0500

    updating submods, removing wp and adding as composer asset

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,7 +3,7 @@
 ############################################
 
 set :stage, :staging
-set :stage_url, "http://10.t3inf.com"
+set :stage_url, "http://t3inf.com"
 server "10.t3inf.com", user: "tripl3inf", roles: %w{web app db}
 set :deploy_to, "/srv/client/ten10"
 
