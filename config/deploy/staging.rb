a987998a0d8d5b1017c7bc03ecadb09edc820533
Alex Aloia <tripl3infinity@gmail.com>
2014-08-10 15:26:02 -0500

    updating staging deploy config

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,7 +4,7 @@
 
 set :stage, :staging
 set :stage_url, "http://10.t3inf.com"
-server "t3inf.com", user: "tripl3inf", roles: %w{web app db}
+server "10.t3inf.com", user: "tripl3inf", roles: %w{web app db}
 set :deploy_to, "/srv/client/ten10"
 
 ############################################
