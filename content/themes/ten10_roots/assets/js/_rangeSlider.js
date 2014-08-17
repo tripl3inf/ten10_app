8c4db777c4d01b46a58b606f4be3998285ec9dee
Alex Aloia <tripl3infinity@gmail.com>
2014-08-16 21:24:12 -0500

    moving riks tollerance script to own file

--- content/themes/ten10_roots/assets/js/_rangeSlider.js
+++ content/themes/ten10_roots/assets/js/_rangeSlider.js
@@ -4,3 +4,49 @@
 
 
 
+    $(function () {
+
+        var $document = $(document),
+            selector = '[data-rangeslider]',
+            $element = $(selector);
+
+        // Example functionality to demonstrate a value feedback
+        function valueOutput(element) {
+            var value = element.value,
+                output = element.parentNode.getElementsByTagName('output')[0];
+            output.innerHTML = value;
+        }
+
+        for (var i = $element.length - 1; i >= 0; i--) {
+            valueOutput($element[i]);
+        }
+
+        $document.on('change', 'input[type="range"]', function (e) {
+            valueOutput(e.target);
+        });
+
+
+        // Basic rangeslider initialization
+        $element.rangeslider({
+
+            // Deactivate the feature detection
+            polyfill: false,
+
+            // Callback function
+            onInit: function () {
+            },
+
+            // Callback function
+            onSlide: function (position, value) {
+                console.log('onSlide');
+                console.log('position: ' + position, 'value: ' + value);
+            },
+
+            // Callback function
+            onSlideEnd: function (position, value) {
+                console.log('onSlideEnd');
+                console.log('position: ' + position, 'value: ' + value);
+            }
+        });
+
+    });
