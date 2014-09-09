02d2ecfa9b9dca10c1d9a836fe21617d9d4d1e72
Alex Aloia <tripl3infinity@gmail.com>
2014-09-09 00:33:12 -0500

    adding projects template

---
+++ content/themes/ten10_roots/templates/template-projects.php
@@ -0,0 +1,98 @@
+<?php
+/*
+Template Name: Projects Page
+*/
+header('Content-Type: image/jpg');
+
+global $post;
+if ( ! empty( $post ) ) {
+  $args        = array( 'post_type' => 'cpt_project', 'posts_per_page' => - 1 );
+  $loop        = new WP_Query( $args );
+
+//  $img = get_post_meta( $post->ID, 'upload_slider_img', true );
+  $img = get_post_meta($post_id, 'upload_slider_img');
+
+}
+?>
+
+
+<?php while (have_posts()) :the_post(); ?>
+<div class="archive-projects">
+
+
+<?php endwhile; ?>
+
+  <?php
+  dynamic_sidebar('sidebar-subhead'); ?>
+
+  <section class="content-wrap">
+    <div class="content">
+      <article class="entry-content">
+        <?php
+        while ( $loop->have_posts() ) :
+        $loop->the_post();
+        ?>
+
+
+
+
+        <ul class="project_list">
+
+
+            <li class="project_info">
+              <div class="thumb_wrap">
+                <a href="<?php echo the_permalink(); ?>">
+                  <?php the_post_thumbnail( 'archive-project-thumb' ); ?>
+                </a>
+              </div>
+
+              <ul>
+                <li class="project_title">
+                  <?php the_title(); ?>
+                </li>
+
+                <li>
+                  <span>Director:</span>
+                  <?php echo get_post_meta( $post->ID, 'project_director', true ); ?>
+                </li>
+
+
+                <li>
+                  <span>Producer:</span>
+                  <?php echo get_post_meta( $post->ID, 'project_producer', true ); ?>
+                </li>
+
+                <li class="project_trailer_link">
+                  <a href="<?php echo get_post_meta( $post->ID, 'project_trailer_url',
+                    true ); ?>" class="btn btn-default btn-xs">
+                    Watch this Trailer
+                  </a>
+                </li>
+
+                <li class="project_desc"><p><?php echo get_post_meta( $post->ID, 'project_desc', true ); ?></p></li>
+              </ul>
+
+            </li>
+        </ul>
+
+
+
+        <?php
+          //the_excerpt();
+        endwhile;
+        ?>
+      </article>
+    </div>
+  </section>
+
+
+</div>
+
+
+
+
+
+
+
+
+
