deb0fb849f189d6edde79f3921616f0c73f7c4f9
Alex Aloia <tripl3infinity@gmail.com>
2014-09-12 09:33:41 -0500

    created custom post type slider & widget -- replace homepage slider

--- content/themes/ten10_roots/piklist/parts/templates/add-more-featured.php
+++
@@ -1,1 +0,0 @@
-<?php echo $data['slider_panels']; ?>
--- content/themes/ten10_roots/piklist/parts/templates/slider-main-template.php
+++ content/themes/ten10_roots/piklist/parts/templates/slider-main-template.php
@@ -1,14 +1,30 @@
 <?php
-//echo $data['page_link'];
 
-//echo $data['tier_name'];
-
-$images = $data['upload_media'];
-
-foreach ($images as $img ) {
-//echo '<p> test = ' . $img . '</p>';
+// slider img var
+if ( ! empty( $data['upload_media_slider_img'] ) ) {
+  $images = $data['upload_media_slider_img'];
+  foreach ( $images as $img ) {
+    $img = $img;
+  }
 }
 
+// slider page link var
+if ( ! empty( $data['page_link'] ) ) {
+  $page_link = $data['page_link'];
+}
+
+
 ?>
-<img src="<?php echo wp_get_attachment_url($img) ?>"/>
+
+<div class="item">
+<a href="<?php echo get_permalink( $page_link ); ?>">
+  <img src="<?php echo wp_get_attachment_url( $img ) ?>"/>
+</a>
+</div>
+
+
+
+
+
+
 
